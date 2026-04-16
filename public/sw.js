1330825e6c20a9524feb05face50028dc5d7de99
Mnene <mnene1100@gmail.com>
2026-04-16 07:36:55 +0000

    I'm hosting my app using vercel

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,14 @@
 
 const CACHE_NAME = 'nexo-v1';
-const ASSETS_TO_CACHE = [
+const ASSETS = [
   '/',
-  '/manifest.json'
+  '/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
